fcd9bb83c1ab1b22ad82d867511216cb27af655c
Krishna Teja Nanduri <krishnateja.nanduri@gmail.com>
2017-10-18 13:32:44 -0700

    Programs done in the last 2 days excluding a couple

---
+++ InterviewBit/Math/Prime Sum.cpp
@@ -0,0 +1,35 @@
+// https://www.interviewbit.com/problems/prime-sum/
+
+vector<int> Solution::primesum(int A) {
+    
+    vector<bool> val;
+    vector<int> sol;
+    // unordered_map<int, bool> map;
+    
+    int i;
+    
+    for(i = 0; i < A; i++){
+        val.push_back(true);
+    }
+    
+    for(i = 2; i <= sqrt(A); i++){
+        if(val[i] == true){
+            for(int j = 2; i*j <= A; j++){
+                val[i*j] = false;
+            }
+        }
+    }
+    
+    for(i = 2; i < A; i++){
+        if(val[i] == true){
+            if(val[A - i] == true){
+                sol.push_back(i);
+                sol.push_back(A-i);
+                return sol;
+            }
+        }
+    }
+       
+   return sol;
+
+}
