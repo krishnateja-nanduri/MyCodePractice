3fb036170e91253d94ef3dc604ab0205300be150
Krishna Teja Nanduri <krishnateja.nanduri@gmail.com>
2017-12-31 19:06:01 -0800

    commits for 30th and 31st December, 2017

---
+++ HackerRank/Dynamic Programming/Longest Increasing Subsequence.cpp
@@ -0,0 +1,43 @@
+//https://www.hackerrank.com/challenges/longest-increasing-subsequent/problem
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */  
+    int N;
+	cin>>N;
+	int *input = new int[N];
+	vector<int> lis; // lis[i] = the last value in the longest increasing subsequence (lis) of length i
+	for(int i = 0; i < N; i++) {
+		cin>>input[i];
+	}
+
+	// Main algorithm
+	lis.push_back(input[0]);
+	for(int i = 1; i < N; i++) {
+		int upperBoundIndex = (upper_bound(lis.begin(), lis.end(), input[i]) - lis.begin());
+		if(upperBoundIndex == lis.size()) {
+			// Additional check to deal with the "strictly increasing" condition 
+			if(input[i] > lis[lis.size()-1]) {
+				lis.push_back(input[i]);
+			}
+		} else {
+			if(upperBoundIndex == 0) {
+				lis[upperBoundIndex] = input[i];
+			}
+			// Additional check to deal with the "strictly increasing" condition 
+			if(input[i] > lis[upperBoundIndex-1]) {
+				lis[upperBoundIndex] = input[i];
+			}
+		}
+	}
+	cout<<lis.size();
+    return 0;
+}
+
